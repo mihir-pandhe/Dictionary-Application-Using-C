87908593b235d016844ea78341f19666e4cd861d
Harsh Pandhe <pandhemihir@gmail.com>
2024-07-19 18:34:37 +0530

    Added Function to Add New Words

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_WORDS 5
+#define MAX_WORDS 100
 #define MAX_WORD_LENGTH 50
 #define MAX_DEFINITION_LENGTH 200
 
@@ -18,8 +18,10 @@ DictionaryEntry dictionary[MAX_WORDS] = {
     {"elephant", "A large herbivorous mammal with a trunk, native to Africa and southern Asia."}
 };
 
+int wordCount = 5;
+
 void lookupWord(char *word) {
-    for (int i = 0; i < MAX_WORDS; i++) {
+    for (int i = 0; i < wordCount; i++) {
         if (strcmp(dictionary[i].word, word) == 0) {
             printf("Definition: %s\n", dictionary[i].definition);
             return;
@@ -28,10 +30,47 @@ void lookupWord(char *word) {
     printf("Word not found in dictionary.\n");
 }
 
+void addWord(char *word, char *definition) {
+    if (wordCount >= MAX_WORDS) {
+        printf("Dictionary is full. Cannot add more words.\n");
+        return;
+    }
+    strcpy(dictionary[wordCount].word, word);
+    strcpy(dictionary[wordCount].definition, definition);
+    wordCount++;
+    printf("Word added successfully.\n");
+}
+
 int main() {
+    int choice;
     char word[MAX_WORD_LENGTH];
-    printf("Enter a word to look up: ");
-    scanf("%s", word);
-    lookupWord(word);
+    char definition[MAX_DEFINITION_LENGTH];
+
+    while (1) {
+        printf("1. Look up word\n2. Add word\n3. Exit\nEnter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1:
+                printf("Enter a word to look up: ");
+                scanf("%s", word);
+                lookupWord(word);
+                break;
+            case 2:
+                printf("Enter the word: ");
+                scanf("%s", word);
+                getchar();
+                printf("Enter the definition: ");
+                fgets(definition, MAX_DEFINITION_LENGTH, stdin);
+                definition[strcspn(definition, "\n")] = '\0';
+                addWord(word, definition);
+                break;
+            case 3:
+                return 0;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
     return 0;
 }
